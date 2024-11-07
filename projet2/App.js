e0ff9e082d2ec6996e43865895e0cc3acb0254b3
lucas <lucas.laliche@gmail.com>
2024-11-07 15:26:55 +0100

    meteo

--- projet2/App.js
+++ projet2/App.js
@@ -6,8 +6,8 @@ import ToDoApp from './components/todo';
 const App = () => {
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <ToDoApp/>
-      {/* <Weather/> */}
+      {/* <ToDoApp/> */}
+      <Weather/>
     </SafeAreaView>
   );
 };
